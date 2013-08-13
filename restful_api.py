9456d72d25e77b07578ed2ca85fbab684e63614d
Phil Opaola <philopaola@percolate.com>
2013-08-13 13:04:48 -0400

    Add support for multiple headers in the request

--- restful_api.py
+++ restful_api.py
@@ -2,6 +2,7 @@ from flask import (
     Flask,
     url_for,
     request,
+    json,
 )
 
 app = Flask(__name__)
@@ -38,6 +39,23 @@ def api_echo():
 
     if request.method in HTTP_VERBS:
         return echo_request_method(request.method)
+
+
+@app.route('/messages', methods=HTTP_VERBS[1:2])
+def api_message():
+    if request.headers['Content-Type'] == 'text/plain':
+        return 'Text Message: ' + request.data
+
+    elif request.headers['Content-Type'] == 'application/json':
+        return 'JSON Message: ' + json.dumps(request.json)
+
+    elif request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('./binary', 'wb')
+        f.write(request.data)
+        f.close()
+        return 'Binary message written!'
+    else:
+        return '415 Unsupported Media Type ;)'
 
 
 if __name__ == '__main__':
