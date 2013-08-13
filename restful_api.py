8fba7c3d28616bbf05705a25d809e3f12aec9cf4
Phil Opaola <philopaola@percolate.com>
2013-08-13 12:51:48 -0400

    REST api server can handle multiple requests

--- restful_api.py
+++ restful_api.py
@@ -5,6 +5,7 @@ from flask import (
 )
 
 app = Flask(__name__)
+HTTP_VERBS = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
 
 
 @app.route('/')
@@ -28,6 +29,16 @@ def api_hello():
         return 'Hello ' + request.args['name']
     else:
         return 'Hello John Doe'
+
+
+@app.route('/echo', methods=HTTP_VERBS)
+def api_echo():
+    def echo_request_method(method):
+        return 'ECHO: %s\n' % method
+
+    if request.method in HTTP_VERBS:
+        return echo_request_method(request.method)
+
 
 if __name__ == '__main__':
     app.run()
